1ace3f7dc53664330d3c430bfc1e30c80a8cdad7
hedin <dima@unest.co>
2024-06-30 13:57:09 +0100

    [feature] Added APIs code with update README

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # big-csv-processor
-Read, process, write big .csv file - Async, FastAPI, Celery
+Read, process, write big .csv files - FastAPI, Celery, Pandas
+
+## How to set up the project
+1) Clone the repository from GitHub
+2) Open project directory
+3) Write next command in Terminal:
+   1) ```pipenv --python 3.10```
+   2) ```pipenv shell```
+   3) ```pip install pipenv```
+4) Configure your interpreter in IDE
+5) Use command ```pipenv update``` to lock and sync dependencies
+
+## How to run
+
+#### Start the Celery worker:
+```bash
+celery -A main.celery worker --loglevel=info
+```
+
+#### Run the FastAPI server:
+```bash
+uvicorn main:app --reload
+```
+
+## How to try
+
+#### Upload a File Using curl
+```bash
+curl -X POST "http://127.0.0.1:8000/process-file/" -H "Content-Type: application/json" -d '{"file_path": "/Users/hedin/PycharmProjects/big-csv-processor/uploads/example_input_file.csv"}'
+```
+For running this command on another machine, you should change path to yours
+
+#### Download the Result
+```bash
+curl "http://127.0.0.1:8000/download-result/e028042c-9cd5-4b00-abf4-d948b79e7461"
+```
+For downloading this info properly, you need to get a task_id from the first curl and change the last part of curl to your task_id
---
+++ main.py
@@ -0,0 +1,67 @@
+from fastapi import FastAPI, BackgroundTasks
+from fastapi.responses import FileResponse
+from celery import Celery
+import os
+import uuid
+import dask.dataframe as dd
+from pydantic import BaseModel
+
+
+app = FastAPI()
+
+# Configure Celery with Redis as task broker
+celery = Celery(__name__, broker='redis://localhost:6379/0', backend='redis://localhost:6379/0')
+
+# Define prefix and directory for result
+RESULT_FILE_PREFIX = '_example_output_file.csv'
+RESULTS_DIR = os.path.join(os.getcwd(), 'results')
+
+# Create directory if it does not exist
+os.makedirs(RESULTS_DIR, exist_ok=True)
+
+
+# pydantic class for file path, can be extended with url, for example
+class FileSource(BaseModel):
+    file_path: str | None = None
+
+
+@app.post("/process-file/")
+async def schedule_file_for_processing(file_source: FileSource, background_tasks: BackgroundTasks):
+    path_to_file = file_source.file_path
+
+    # Generate a unique task ID
+    task_id = str(uuid.uuid4())
+
+    # Schedule the processing task
+    background_tasks.add_task(process_file_task, path_to_file, task_id)
+
+    return {'task_id': task_id}
+
+
+@app.get('/download-result/{task_id}')
+async def download_result(task_id: str):
+    file_name = f'{task_id}{RESULT_FILE_PREFIX}'
+    # Check if the result file exists
+    result_path = os.path.join(RESULTS_DIR, file_name)
+    if os.path.exists(result_path):
+        return FileResponse(result_path, media_type='text/csv', filename=file_name)
+    else:
+        return {'status': 'Processing'}
+
+
+@celery.task
+def process_file_task(file_url, task_id):
+    output_path = os.path.join(RESULTS_DIR, f'{task_id}{RESULT_FILE_PREFIX}')
+    process_large_csv(file_url, output_path)
+
+
+# The process_large_csv function from the first part of the task
+def process_large_csv(path_to_file, output_file):
+    # Read the CSV file in chunks using Dask
+    df = dd.read_csv(path_to_file)
+
+    # Group by "Song" and "Date" and sum the "Number of Plays"
+    result = df.groupby(['Song', 'Date']).sum().reset_index()
+
+    # Write the result to output file. In this case it's CSV, but we can use s3 for example
+    result.to_csv(output_file, single_file=True, index=False)
